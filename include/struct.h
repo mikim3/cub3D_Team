dc8fa764d83743265a323c7ff6243e169c3a13b4
soyeong-lee <soylee@student.42seoul.kr>
2023-03-31 16:23:29 +0900

    norm

--- include/struct.h
+++ include/struct.h
@@ -13,7 +13,7 @@
 #ifndef STRUCT_H
 # define STRUCT_H
 
-typedef struct	s_img
+typedef struct s_img
 {
 	void		*img;
 	int			*data;
--- srcs/ray_util.c
+++ srcs/ray_util.c
@@ -12,7 +12,7 @@
 
 #include "cub3D.h"
 
-double normalize_angle(double angle)
+double	normalize_angle(double angle)
 {
 	if (angle >= 0)
 	{
@@ -24,22 +24,19 @@ double normalize_angle(double angle)
 		while (angle <= 0)
 			angle += M_PI * 2;
 	}
-	return angle;
+	return (angle);
 }
 
-double distance_between_points(double x1, double y1, double x2, double y2)
+double	distance_between_points(double x1, double y1, double x2, double y2)
 {
-	return sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+	return (sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1)));
 }
 
 void	cal_distance(t_cub *cub, t_dpable_ray *horz_or_vert)
 {
-	// printf("horz_or_vert-> distance == %lf \n", horz_or_vert->distance);
-
 	if (horz_or_vert->found_wallhit == TRUE)
-		horz_or_vert->distance = distance_between_points(cub->player.x, cub->player.y, horz_or_vert->wall_hitx, horz_or_vert->wall_hity);
+		horz_or_vert->distance = distance_between_points(cub->player.x, \
+			cub->player.y, horz_or_vert->wall_hitx, horz_or_vert->wall_hity);
 	else
 		horz_or_vert->distance = 999999999999;
-		// horz_or_vert->distance = DBL_MAX;
-	// 벽을 찾지 못할 경우 수평경계좌표와 수직경계좌표를 비교할때 선택되지 않도록
 }
